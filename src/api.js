d7337c0fcbb4c2352b702445d7a7247b176617d9
Korostelev Konstantin <korostel97@tutamail.com>
2025-03-21 19:04:39 +0400

    router part 1

---
+++ src/api.js
@@ -0,0 +1,24 @@
+import { redirect } from "react-router-dom";
+
+import todos from "./todos";
+
+export function getTodos() {
+  return todos;
+}
+
+export async function addTodo({ request }) {
+  const fd = await request.formData();
+  const date = new Date();
+
+  const newTodo = {
+    title: fd.get('title'),
+    desc: fd.get('desc'),
+    image: fd.get('image'),
+    done: false,
+    createdAt: date.toLocaleString(),
+    key: date.getTime(),
+  };
+
+  todos.push(newTodo);
+  return redirect('/');
+}
