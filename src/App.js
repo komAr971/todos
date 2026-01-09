346b71b671e9cbc92d7d912e47849cbead76119f
Korostelev Konstantin <korostel97@tutamail.com>
2026-01-09 16:02:50 +0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import { useState } from "react";
 
-import TodoList from "./TodoList";
-import TodoAdd from "./TodoAdd";
-import initialTodos from "./todos";
+import { Outlet, NavLink } from "react-router-dom";
 
 
 export default function App() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const setDone = key => {
-    const newTodos = [...todos];
-    const deed = newTodos.find(current => current.key === key);
-    if (deed) {
-      deed.done = true;
-    }
-    setTodos(newTodos);
-  }
-
-  const del = key => {
-    const newTodos = todos.filter(current => current.key !== key);
-    setTodos(newTodos);
-  }
-
-  const add = deed => {
-    setTodos([...todos, deed]);
+  const handleBurgerClick = evt => {
+    evt.preventDefault();
+    setShowMenu(!showMenu);
   }
 
   return(
     <div className="container">
       <nav className="navbar is-light">
         <div className="navbar-brand">
-          <span className="navbar-item is-uppercase">
+          <NavLink
+            to="/"
+            className={ (isActive) => 'navbar-item is-uppercase' + (isActive ? ' is-active' : '') }
+          >
             Todos
-          </span>
+          </NavLink>
+          <a href="/" className={showMenu ? 'navbar-burger is-active' : 'navbar-burger' } onClick={handleBurgerClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+        <div className={showMenu ? 'navbar-menu is-active' : 'navbar-menu'} onClick={handleBurgerClick}>
+          <div className="navbar-start">
+            <NavLink
+              to="/add"
+              className={ (isActive) => 'navbar-item' + (isActive ? ' is-active' : '') }
+            >
+              Создать дело
+            </NavLink>
+          </div>
         </div>
       </nav>
       <main className="content px-6 py-6">
-        <TodoList list={todos} setDone={setDone} del={del}/>
-        <TodoAdd add={add}/>
+        <Outlet />
       </main>
     </div>
   )
