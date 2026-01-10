ad7dfae8f50ccaf59d7d4b29a5951a356e2485ea
Korostelev Konstantin <korostel97@tutamail.com>
2026-01-10 12:49:20 +0400

    add stats in console

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import router from './router.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
