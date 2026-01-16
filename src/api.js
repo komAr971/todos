68f8ff9f989f94330965bbcbaabbf1ac6e558a31
Korostelev Konstantin <korostel97@tutamail.com>
2026-01-16 16:17:14 +0400

    add database actions

--- src/api.js
+++ src/api.js
@@ -1,20 +1,29 @@
 import { redirect } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getDatabase, ref, push, set, query, get, remove } from "firebase/database";
 
-import todos from './todos.js';
 import firebaseApp from './firebase.js';
 
 const auth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
+
+function getUserId() {
+  if (auth.currentUser) {
+    return auth.currentUser.uid;
+  } else {
+    return window.localStorage.getItem('user-id');
+  }
+}
 
 export async function register({ request }) {
   const fd = await request.formData();
   try {
-    //const oUC = 
-    await createUserWithEmailAndPassword(
+    const cr = await createUserWithEmailAndPassword(
       auth,
       fd.get('email'),
       fd.get('password')
     );
+    window.localStorage.setItem('user-id', cr.user.uid);
     return redirect('/')
   } catch (err) {
     return err.code;
@@ -28,7 +37,8 @@ export function setStateChangeHandler(func) {
 export async function login({ request }) {
   const fd = await request.formData();
   try {
-    await signInWithEmailAndPassword(auth, fd.get('email'), fd.get('password'))
+    const cr = await signInWithEmailAndPassword(auth, fd.get('email'), fd.get('password'))
+    window.localStorage.setItem('user-id', cr.user.uid);
     return redirect('/')
   } catch (err) {
     return err.code;
@@ -37,23 +47,37 @@ export async function login({ request }) {
 
 export async function logout() {
   await signOut(auth);
+  window.localStorage.removeItem('user-id');
   return redirect('/login');
 }
 
-export function getTodos() {
-  return todos;
+export async function getTodos() {
+  const currentUserId = getUserId();
+  const r = ref(database, `users/${currentUserId}/todos`);
+  const q = query(r);
+  const s = await get(q);
+  const res = [];
+  s.forEach((doc) => {
+    const __todo = doc.val();
+    __todo.key = doc.key;
+    res.push(__todo);
+  })
+  return res;
 }
 
-export function getTodo({ params }) {
-  const key = +params.key;
-  const todo = todos.find(current => current.key === key);
-  if (!todo) {
-    throw new Error()
+export async function getTodo({ params }) {
+  const currentUserId = getUserId();
+  const r = ref(database, `users/${currentUserId}/todos/${params.key}`);
+  const q = query(r);
+  const s = await get(q);
+  if (!s.exists()) {
+    throw new Error();
   }
-  return todo;
+  return s.val();
 }
 
 export async function addTodo({ request }) {
+  const currentUserId = getUserId();
   const fd = await request.formData();
   const date = new Date();
   const newTodo = {
@@ -62,19 +86,21 @@ export async function addTodo({ request }) {
     image: fd.get('image'),
     done: false,
     createdAt: date.toLocaleString(),
-    key: date.getTime()
   };
-  todos.push(newTodo);
+  const db = ref(database, `users/${currentUserId}/todos`);
+  const r = await push(db);
+  await set(r, newTodo);
   return redirect('/');
 }
 
-export function actTodo({ params, request }) {
-  const key = +params.key;
-  const todoIndex = todos.findIndex(current => current.key === key);
+export async function actTodo({ params, request }) {
+  const currentUserId = getUserId();
   if (request.method === 'PATCH') {
-    todos[todoIndex].done = true;
+    const r = ref(database, `users/${currentUserId}/todos/${params.key}/done`);
+    set(r, true);
   } else if (request.method === 'DELETE') {
-    todos.splice(todoIndex, 1);
+    const r = ref(database, `users/${currentUserId}/todos/${params.key}`);
+    remove(r);
   }
   return redirect('/')
 }
